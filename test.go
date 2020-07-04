0644ef6bc114b1112149002f7de556308a4bc806
Mek Obchey <aobchey00@uvic.ca>
2020-07-03 21:37:45 -0700

    Added latency measurement for crawl function

--- test.go
+++ test.go
@@ -1,34 +1,50 @@
 package main
 
 import (
-	WG "csc462/DistributedWebCrawler/webgraph"
+	"fmt"
+	"net/url"
 )
-var g WG.Webgraph
-
-func fillGraph() {
-	nA := WG.Node{Url:"A"}
-	nB := WG.Node{Url:"B"}
-	nC := WG.Node{Url:"C"}
-	nD := WG.Node{Url:"D"}
-	nE := WG.Node{Url:"E"}
-	nF := WG.Node{Url:"F"}
-	g.AddNode(&nA)
-	g.AddNode(&nB)
-	g.AddNode(&nC)
-	g.AddNode(&nD)
-	g.AddNode(&nE)
-	g.AddNode(&nF)
-
-	g.AddEdge(&nA, &nB)
-	g.AddEdge(&nA, &nC)
-	g.AddEdge(&nB, &nE)
-	g.AddEdge(&nC, &nE)
-	g.AddEdge(&nE, &nF)
-	g.AddEdge(&nD, &nA)
-}
 
 
 func main() {
-	fillGraph()
-	g.String()
+
+	urls := [...] string {
+		"http://1867.com/",
+		"http://www.facebook.com/",
+		"https://www.facebook.com/",
+		"http://www.cs.ubc.ca/~bestchai/teaching/../teaching/././../teaching/cs416_2016w2/assign5/index.html",
+		"http://www.cs.ubc.ca/~bestchai/teaching/cs416_2016w2/assign5/index.html",
+	}
+
+	fmt.Println("*** Get Domain Name Test *** ")
+	for _,u := range urls {
+		fmt.Println(getDomainName(u))
+	}
+
+	fmt.Println("\n*** Remove Relative Path Test ***")
+	for _,u := range urls {
+		fmt.Println(getAbsolutePath(u))
+	}
+
+	mmap := make(map[string][]string) // maps each worker's ip:port to addresses
+	wip := "127.0.0.1:3000"
+	arr := []string{urls[0]}
+	mmap[wip] = []string{urls[1]}
+	if mmap[wip] == nil {
+		mmap[wip] = arr
+	} else {
+		mmap[wip] = append(mmap[wip], urls[1])
+	}
+	fmt.Println(mmap)
+}
+
+func getDomainName(uri string) string {
+	u, _ := url.Parse(uri)
+	return u.Host
+}
+
+func getAbsolutePath(uri string) string {
+	u, _ := url.Parse(uri)
+	base, _ := url.Parse(getDomainName("http://example.com/directory/"))
+	return base.ResolveReference(u).String()
 }
